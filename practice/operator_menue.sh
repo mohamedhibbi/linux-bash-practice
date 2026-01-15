2d062d2845e96eefff6ad3cedc17fa5cee7ba1e0
Mohamed Hibbi <medhibbi7@gmail.com>
2026-01-15 06:44:54 +0100

    add some changes to comments

--- practice/operator_menue.sh
+++ practice/operator_menue.sh
@@ -1,29 +1,37 @@
-#!/bin/bash 
-# Let's Write a Menue that offers  the Following requirements available disk space, available ram and users currently logged in 
-# and should not be possible to exit from the menue using CTRL-C 
+#!/bin/bash
+# Interactive Bash menu to display basic system information
+# Demonstrates: select, PS3, trap (SIGINT), and case statements
 
-PS3='Enter your choice: ' 
-options=("See Disk Space" "Check Memory" "List current users" "Quit")
+PS3='Enter your choice: '
 
-trap "ignoring ctrl-c" SIGINT
+options=(
+  "See Disk Space"
+  "Check Memory"
+  "List current users"
+  "Quit"
+)
 
-select opt in "${options[@]}" 
+# Prevent exiting the menu using CTRL-C
+trap "echo 'CTRL-C is disabled in this menu'" SIGINT
+
+select opt in "${options[@]}"
 do
-	case $opt in 
-		"See Disk Space")
-			df -h 
-			;;
-		"Check Memory")
-			free -h 
-			;;
-		"List current users")
-			who
-			;;
-		"Quit")
-			break
-			;;
-		*)
-			echo "invalid option $REPLY" 
-			;;
-	esac	
+    case $opt in
+        "See Disk Space")
+            df -h
+            ;;
+        "Check Memory")
+            free -h
+            ;;
+        "List current users")
+            who
+            ;;
+        "Quit")
+            break
+            ;;
+        *)
+            echo "Invalid option: $REPLY"
+            ;;
+    esac
 done
+
