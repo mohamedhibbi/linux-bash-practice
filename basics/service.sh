0a9dce5258e0c877a9f127bce9fcaa9b672b374e
Mohamed Hibbi <medhibbi7@gmail.com>
2026-01-29 16:29:48 +0000

    add some comments # and information about author

--- basics/service.sh
+++ basics/service.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
+################################################################################
+# Script Name: service.sh
+# Author: Mohamed Hibbi
+# Usage: ./service.sh [start|stop|restart|status] <service>
+# Example: ./service.sh restart sshd
+################################################################################
 
+# First argument is the action (start/stop/restart/status)
 ACTION="$1"
+# Second argument is the service name
 SERVICE="$2"
 
+# Check if both arguments are provided
 if [[ -z "$ACTION" || -z "$SERVICE" ]]; then
     echo "Usage: $0 [start|stop|restart|status] <service>"
     exit 1
 fi
 
-# Require root
+# Require root privileges to run systemctl
 if [[ $EUID -ne 0 ]]; then
     echo "Please run as root."
     exit 1
 fi
 
+# Main case statement to handle actions
 case "$ACTION" in
     start)
         echo "Starting $SERVICE..."
-        systemctl start "$SERVICE"
+        systemctl start "$SERVICE" && echo "Service started successfully."
         ;;
     stop)
         echo "Stopping $SERVICE..."
-        systemctl stop "$SERVICE"
+        systemctl stop "$SERVICE" && echo "Service stopped successfully."
         ;;
     restart)
         echo "Restarting $SERVICE..."
-        systemctl restart "$SERVICE"
+        systemctl restart "$SERVICE" && echo "Service restarted successfully."
         ;;
     status)
+        echo "Status of $SERVICE:"
         systemctl status "$SERVICE"
         ;;
     *)
@@ -36,3 +47,4 @@ case "$ACTION" in
         exit 1
         ;;
 esac
+
